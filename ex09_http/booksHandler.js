57d5430ef2588a8cffad596e6d2eda8cdec84f89
Ulyana <ulanas599@gmail.com>
2026-04-12 12:12:23 +0300

    update ex09

--- ex09_http/booksHandler.js
+++ ex09_http/booksHandler.js
@@ -1,71 +1,114 @@
+import DataSource from "./dataSource.js";
+const dataSource = new DataSource('db/database.json');
+
 export const booksHandler = (req, res) => {
 
-  const {method, url} = req;
+  const { method, url } = req;
 
-  console.log('url == ', url)
+  // console.log('url == ', url);
+  const urlSplitted = url.split('?');
+  const urlString = urlSplitted[0];
+  const queryString = urlSplitted[1]; // FIXME:
 
-      const urlSplitted = url.split('?');
-      const urlString  = urlSplitted[0];
-      const queryString = urlSplitted[1];
+  const urlArr = urlString.split('/');
 
-      const urlArr = urlString.split('/');
-      //console.log('urlArr', urlArr, urlArr.length);
+  // console.log('urlArr', urlArr, urlArr.length);
 
-      let id = null;
-        if(urlArr.length ===  4) {
-        const id = +urlArr[urlArr.length-1];
+  let id = null;
+  if (urlArr.length === 4) {
+    id = +urlArr[urlArr.length - 1]; // FIXME: 
+  }
 
-  switch(method) {
+  let re = null;
+  let bodyText = '';
+
+  switch (method) {
     case 'POST':
-      res.writeHead(201, {'Content-Type': 'application/json'});
-      res.end(`{
-    "id": 1,
-    "name": "Преступление и наказание",
-    "author": "Ф. М. Достоевский",
-    "description": "Это произвдедение было написано..."
-}`);
+      bodyText = '';
+      req.on('data', (chunk) => {
+        console.log('body data chunk detected!');
+        bodyText += chunk;
+      });
+
+      req.on('end', () => {
+
+        try {
+          console.log('body end detected!', bodyText);
+          const body = JSON.parse(bodyText);
+          console.log('body', body);
+
+          re = JSON.stringify(dataSource.create(body));
+          res.writeHead(201, { 'Content-Type': 'application/json' });
+          res.end(re);
+
+        } catch (e) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(
+            {
+              status: "error",
+              message: e.toString()
+            })
+          );
+        }
+      });
       return;
     case 'GET':
-      if(id) {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(`{
-    "id": 1,
-    "name": "Преступление и наказание",
-    "author": "Ф. М. Достоевский",
-    "description": "Это произвдедение было написано..."
-}`);
+      if (id) {
+        re = JSON.stringify(dataSource.getOne(id));
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(re);
       } else {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(`[{
-    "id": 1,
-    "name": "Преступление и наказание",
-    "author": "Ф. М. Достоевский",
-    "description": "Это произвдедение было написано..."
-}]`);
+        re = JSON.stringify(dataSource.getAll());
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        res.end(re);
       }
       return;
     case 'PATCH':
     case 'PUT':
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.end(`{
-    "id": 1,
-    "name": "Преступление и наказание!",
-    "author": "Ф. М. Достоевский",
-    "description": "Это произвдедение было написано..."
-}`);
+      bodyText = '';
+      req.on('data', (chunk) => {
+        console.log('body data chunk detected!');
+        bodyText += chunk;
+      });
+
+      req.on('end', () => {
+
+        try {
+          console.log('body end detected!', bodyText);
+          const body = JSON.parse(bodyText);
+          console.log('body', body);
+
+          dataSource.update(id, body);
+          re = JSON.stringify(dataSource.getOne(id));
+          res.writeHead(200, { 'Content-Type': 'application/json' });
+          res.end(re);
+
+        } catch (e) {
+          res.writeHead(500, { 'Content-Type': 'application/json' });
+          res.end(JSON.stringify(
+            {
+              status: "error",
+              message: e.toString()
+            })
+          );
+        }
+      });  
       return;
     case 'DELETE':
-      res.writeHead(204, {'Content-Type': 'application/json'});
+      dataSource.delete(id);
+      res.writeHead(204);
       res.end(null);
       return;
-  };
+  }
 
-res.writeHead(500, {'Content-Type': 'application/json'});
+  res.writeHead(501, { 'Content-Type': 'application/json' });
   res.end(JSON.stringify(
-      {
-        status: "error",
-        message: "method not implemented"
-      }
-    )
+    {
+      status: "error",
+      message: "method not implemented"
+    })
   );
-}}
+
+
+
+};
--- ex09_http/usersHandler.js
+++ ex09_http/usersHandler.js
@@ -1,10 +1,9 @@
 export const usersHandler = (req, res) => {
-res.writeHead(200, {'Content-Type': 'application/json'});
+  res.writeHead(200, {'Content-Type': 'application/json'});
   res.end(JSON.stringify(
-      {
-        status: "TBD",
-        message: "usersHandler under construction!"
-      }
-    )
+    {
+      status: "TBD",
+      message: "usersHandler under construction!"
+    })
   );
-}
+};
