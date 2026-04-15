06dd8af097d01b0a60eeda5cb6b472425dcbee44
Ulyana <ulanas599@gmail.com>
2026-04-15 21:24:39 +0300

    add ex10

---
+++ ex10_express/app.js
@@ -0,0 +1,26 @@
+import express from 'express';
+import path from 'node:path';
+import cookieParser from 'cookie-parser';
+import logger from 'morgan';
+
+import { fileURLToPath } from 'node:url';
+import { dirname } from 'node:path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+import indexRouter from './routes/index.js';
+import usersRouter from './routes/users.js';
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+export default app;
